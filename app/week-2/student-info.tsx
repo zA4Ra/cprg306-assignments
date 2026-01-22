52437fd3910ab5998253ea14121d0925f1377338
Zaara <zaaraa178@gmail.com>
2026-01-22 15:22:01 -0700

    push

--- app/week-2/student-info.tsx
+++ app/week-2/student-info.tsx
@@ -4,7 +4,8 @@ export default function StudentInfo() {
     return (
         <div>
             <h2>Name : Zaara Ahmad</h2>
-            <h2>Github Repo: https://github.com/zA4Ra/cprg306-assignments.git </h2>
+            <a href="https://github.com/zA4Ra/cprg306-assignments.git">GitHub Repo: https://github.com/zA4Ra/cprg306-assignments.git</a>
+
         </div>
     );
 }
